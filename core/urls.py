9fb02e764f8ab43be7cf621919c77aa477795aa8
Arpit-Rajawat <Arpitsingh62650@gmail.com>
2025-08-14 14:04:04 +0530

    commit

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
    
     path('generate_notes/', views.generate_notes, name='generate_notes'),
 
-    # path('generate_transcript/', views.generate_transcript, name='generate_transcript'),
-   
 ]
